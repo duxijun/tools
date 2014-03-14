47f66a0346dbd5c0710651ce5c553ebd3a344b10
duxijun <duxijun0703@gmail.com>
2014-03-14 11:26:39 +0800

    com_logger.py

--- cloudmanager_ha/com_logger.py
+++ cloudmanager_ha/com_logger.py
@@ -5,16 +5,19 @@ import sys
 import os
 import logging
 import string
+from logging.handlers import RotatingFileHandler
 
+LOG_FILE="/var/log/cloud/cloudmanager_ha.log"
 logger=logging.getLogger()
-handler=logging.FileHandler("/var/log/cloudmanager_ha.log")
+handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes = 1024*1024, backupCount = 5)
 formatter = logging.Formatter('%(asctime)s %(filename)s:%(lineno)d [%(levelname)s] %(message)s')
-#formatter = logging.Formatter('%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-console = logging.StreamHandler()
-console.setFormatter(formatter)
-logging.getLogger('').addHandler(console)  
+
+#console = logging.StreamHandler()
+#console.setFormatter(formatter)
+#logging.getLogger('').addHandler(console)  
+
 logger.setLevel(logging.DEBUG)
 #logger.setLevel(logging.INFO)
 
