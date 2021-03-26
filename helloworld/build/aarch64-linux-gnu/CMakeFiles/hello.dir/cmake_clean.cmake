4a82bd12e03162f640bd212298a4b8992ae4e716
duxijun <duxijun0703@gmail.com>
2021-03-26 09:50:12 +0800

    add demo

---
+++ helloworld/build/aarch64-linux-gnu/CMakeFiles/hello.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello.dir/src/main.c.o"
+  "../../out/aarch64-linux-gnu/hello.pdb"
+  "../../out/aarch64-linux-gnu/hello"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ helloworld/src/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * This is a every simple sample for MiniGUI.
+ * It will create a main window and display a string of "Hello, world!" in it.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main()
+{
+	return 0;
+}
+
+
